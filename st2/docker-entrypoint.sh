3a507d890f3f4c30daf4c7def10cf9f5ee00ec17
李建赢 <lijianying12@gmail.com>
2017-04-26 13:12:26 +0800

    patch ep

--- st2/docker-entrypoint.sh
+++ st2/docker-entrypoint.sh
@@ -12,4 +12,25 @@ sudo openssl req -x509 -newkey rsa:2048 -keyout /etc/ssl/st2/st2.key -out /etc/s
 -days 365 -nodes -subj "/C=US/ST=California/L=Palo Alto/O=StackStorm/OU=Information \
 Technology/CN=$(hostname)"
 
+printf "%s\n" "${CONN_RMQ:?Need to set CONN_RMQ non-empty}"
+printf "%s\n" "${MONGO_HOST:?Need to set MONGO_HOST non-empty}"
+printf "%s\n" "${MONGO_DB:?Need to set MONGO_DB non-empty}"
+printf "%s\n" "${MONGO_PORT:?Need to set MONGO_PORT non-empty}"
+cat >> /etc/st2/st2.conf <<EOF
+[system_user]
+user = root
+ssh_key_file = /root/.ssh/id_rsa
+
+[messaging]
+url = $CONN_RMQ
+
+[ssh_runner]
+remote_dir = /tmp
+
+[database]
+host = $MONGO_HOST
+port = $MONGO_PORT
+db_name = $MONGO_DB
+EOF
+
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
